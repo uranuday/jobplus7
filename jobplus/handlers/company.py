288abd4afb344553da683bfbbe75d145bc2d6a36
qffa <nexthop@live.cn>
2017-12-20 12:31:27 +0800

    fix 21, login can work - qff

---
+++ jobplus/handlers/company.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+
+
+
+company = Blueprint("company", __name__, url_prefix="/company")
+
+
+
+@company.route("/index")
+def index():
+
+    return render_template("company/index.html")
+
+
+
